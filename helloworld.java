97eb781b351211b5ef840abfe3a77cb6ff59d053
Practice <vivekchitchula024@gmail.com>
2023-08-10 09:22:19 +0530

    space added in hello world and new line

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 public class helloworld {
      public class void main(string[] args) {
-         System.out.println("helloworld");
-         System.out.println("newline");
+         System.out.println("hello world");
+         System.out.println("new line");
       }
     }
