8580886a595de5f52662647ca235621e8a5e1f79
Practice <vivekchitchula024@gmail.com>
2023-08-11 06:37:42 +0530

    substration of two numbers

--- Arthematicoperations.java
+++ Arthematicoperations.java
@@ -2,7 +2,7 @@ public class arthematicoperations {
     public static void main(string[] args) {
     int a=10;
     int b=20;
-    int c=a+b;
-   System.out.println("sum of the two numbers is ="  +c);
+    int c=a-b;
+   System.out.println("substraction of the two numbers is ="  +c);
    }
 }      
