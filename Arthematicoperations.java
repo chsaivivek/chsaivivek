507afc5415022c097aee91c7fee735861655f28f
Practice <vivekchitchula024@gmail.com>
2023-08-11 06:33:03 +0530

    adding two numbers

---
+++ Arthematicoperations.java
@@ -0,0 +1,8 @@
+public class arthematicoperations {
+    public static void main(string[] args) {
+    int a=10;
+    int b=20;
+    int c=a+b;
+   System.out.println("sum of the two numbers is ="  +c);
+   }
+}      
